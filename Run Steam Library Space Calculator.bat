5aac72954f00b7b48beaf491e60e4e5ed9011b86
NitroPyridine <121694771+NitroPyridine@users.noreply.github.com>
2025-05-20 23:55:44 +0300

    Update Run Steam Library Space Calculator.bat

--- Run Steam Library Space Calculator.bat
+++ Run Steam Library Space Calculator.bat
@@ -1,22 +1,22 @@
 @echo off
 chcp 65001 >nul
-title Steam Library Space Calculator
+title Steam Library Total Space Calculator
 
 python --version >nul 2>&1
 if errorlevel 1 (
-    echo Python не найден. Установите Python и добавьте его в PATH.
+    echo Python not found. Install Python and add it to the PATH.
     pause
     exit /b
 )
 
-echo Установка зависимостей...
+echo Installing required libraries...
 python -m ensurepip --upgrade >nul 2>&1
 python -m pip install --upgrade pip >nul
 python -m pip install -r requirements.txt
 
-echo Запуск программы...
+echo Running the program...
 python main.py
 
 echo.
-echo Работа завершена. Нажмите любую клавишу для выхода...
+echo Program execution completed. Press any key to exit...
 pause >nul
